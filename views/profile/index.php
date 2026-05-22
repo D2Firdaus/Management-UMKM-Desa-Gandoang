285d12426d82343f4d5b2f9903ff4c2a699ce463
Dede Firdaus <dedefirdausxd@gmail.com>
2026-05-22 23:45:51 +0700

    chore: basic structure for user profile

---
+++ views/profile/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profil UMKM</title>
+</head>
+<body>
+    <?php include_once __DIR__ . '/../layouts/sidebar_user.php'; ?>
+</body>
+</html>
